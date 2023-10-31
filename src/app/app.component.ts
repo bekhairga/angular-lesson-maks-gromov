ee4272961b2120c4efc5839d37843ac8eb614ba0
BekhaIrga <irgashevbehzod@gmail.com>
2023-10-31 06:13:26 +0500

    Usage of @Input and @Output, passing props and events(function)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = 'App Component Hello';
+  expression = false;
+
+  age = 23;
+
+  sayHi(text: string) {
+    // receiving data from child
+    console.log('say hi', text);
+  }
 }
